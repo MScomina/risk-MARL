f17b3972be2d83b0b6d49408b80f2c7332ead5bd
Michele Scomina <mscomina@gmail.com>
2026-04-28 07:28:25 +0200

    Started implementing Risk state logic.

--- environment/risk.py
+++ environment/risk.py
@@ -2,13 +2,16 @@
 # https://gymnasium.farama.org/api/spaces/
 # https://pettingzoo.farama.org/content/environment_creation/
 
+from copy import copy
 import functools
+import random
+
+import numpy as np
+import networkx as nx
 
 import gymnasium
-import numpy as np
 from gymnasium.spaces import Discrete
 from gymnasium.utils import seeding
-
 from pettingzoo import AECEnv
 from pettingzoo.utils import AgentSelector, wrappers
 
@@ -26,6 +29,34 @@ class raw_env(AECEnv):
 
         self.possible_agents = ["player_" + str(r) for r in range(n_agents)]
         self.map_network = graph_utils.generate_graph(map_path)
+        self.territory_armies = {}
+        for territory in list(self.map_network.nodes):
+            self.territory_armies[territory] = [None for _ in self.possible_agents]
+        self.player_armies = [{} for _ in self.possible_agents]
+
+
+    def reset(self, seed=None, options=None):
+
+        self.agents = copy(self.possible_agents)
+        self.timestep = 0
+
+        self.territory_armies = {}
+        for territory in list(self.map_network.nodes):
+            self.territory_armies[territory] = [0 for _ in self.possible_agents]
+
+        observation = {
+            "ownership"     : np.full(G.number_of_nodes(), -1, dtype=np.int32),
+            "troops"        : np.zeros(G.number_of_nodes(), dtype=np.int32),
+            "adjacency"     : nx.to_numpy_array(self.map_network).astype(np.int32),
+            "phase"         : np.array([0], dtype=np.int32)
+        }
+
+        observations = {}
+        for agent in self.agents:
+            observations[agent] = {
+                "observation" : observation,
+                "action_mask" : ...
+            }
 
 
 
@@ -44,10 +75,6 @@ class raw_env(AECEnv):
 
 
     def close(self):
-        pass
-
-
-    def reset(self, seed=None, options=None):
         pass
 
 
